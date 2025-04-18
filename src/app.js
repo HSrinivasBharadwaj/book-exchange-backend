a92a99f39b114d770d6afbe0e9e61ae133dfe849
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-04-18 17:39:58 +0530

    Updated the cors,added the router for profilePage

--- src/app.js
+++ src/app.js
@@ -3,13 +3,21 @@ const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser')
 const authRouter = require('./routes/auth');
+const profileRouter = require('./routes/profile')
 const connectDb = require('./config/database');
 const port = 2222;
 
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}))
 app.use(express.json())
 app.use(cookieParser())
+
 app.use("/",authRouter)
+app.use("/",profileRouter)
 
 
 connectDb().then(() => {
